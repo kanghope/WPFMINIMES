160a2399ff0eb26d95c9223d3ef1dfeff576c594
kanghope <tansang2@naver.com>
2025-12-30 15:48:02 +0900

    커밋1111124234234

--- MiniMES/MiniMes.Client/App.xaml.cs
+++ MiniMES/MiniMes.Client/App.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using MiniMes.Infrastructure.Interfaces;
+using MiniMes.Infrastructure.Services;
+using MiniMes.Client.ViewModels;
+using System;
 using System.Windows;
 
 namespace MiniMes.Client
@@ -9,6 +12,33 @@ namespace MiniMes.Client
     /// </summary>
     public partial class App : Application
     {
+        // 서비스들을 담아두는 바구니(컨테이너)입니다.
+        public static IServiceProvider? ServiceProvider { get; private set; }
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            ServiceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // 1. 서비스 등록 (IWorkOrderService를 요청하면 WorkOrderService를 준다)
+            // AddSingleton: 프로그램이 켜져 있는 동안 딱 하나만 만들어서 공유합니다.
+            services.AddSingleton<IWorkOrderService, WorkOrderService>();
+
+            // PLC 통신 서비스도 등록
+            //services.AddSingleton<PlcCommunicationService>();
+
+            // 2. ViewModel 등록
+            // AddTransient: 필요할 때마다(창을 열 때마다) 새로 만듭니다.
+            services.AddTransient<WorkOrderListViewModel>();
+            services.AddTransient<WorkOrderEditViewModel>();
+            services.AddTransient<WorkResultListViewModel>();
+            services.AddTransient<WorkResultRegisterViewModel>();
+        }
     }
 
 }
+
--- MiniMES/MiniMes.Client/Views/WorkOrderEditView.xaml.cs
+++ MiniMES/MiniMes.Client/Views/WorkOrderEditView.xaml.cs
@@ -1,126 +1,79 @@
 ﻿using MiniMes.Client.ViewModels;
 using System.Collections.Generic;
-
 using System.Linq;
-
 using System.Text;
-
 using System.Threading.Tasks;
-
 using System.Windows;
-
 using System.Windows.Controls;
-
 using System.Windows.Data;
-
 using System.Windows.Documents;
-
 using System.Windows.Input;
-
 using System.Windows.Media;
-
 using System.Windows.Media.Imaging;
-
 using System.Windows.Shapes;
 
-using MiniMes.Client.ViewModels;
-
-
-
 namespace MiniMes.Client.Views
-
 {
-
     /// <summary>
-
-    /// Window1.xaml에 대한 상호 작용 논리
-
+    /// [작업지시 등록/수정 팝업창의 비하인드 코드]
+    /// 사용자가 버튼을 눌렀을 때의 동작(창 닫기, 메시지 띄우기 등)을 담당합니다.
     /// </summary>
-
     public partial class WorkOrderEditView : Window
-
     {
-
+        // 생성자: 창이 만들어질 때 실행됩니다.
         public WorkOrderEditView()
-
         {
-
-            InitializeComponent();
-
+            InitializeComponent(); // XAML 화면 디자인을 불러옵니다.
         }
 
-
-
+        // ---------------------------------------------------------------------
+        // [저장 버튼] 클릭 시 실행
+        // ---------------------------------------------------------------------
         private void SaveButton_Click(object sender, RoutedEventArgs e)
-
         {
-
-            // DataContext를 ViewModel 타입으로 가져옵니다.
-
-            // DataContext가 Edit ViewModel인지 확인
-
+            // 이 화면에 연결된 '두뇌(DataContext)'가 WorkOrderEditViewModel인지 확인합니다.
+            // 'vm'이라는 별명으로 부르기 시작합니다.
             if (DataContext is WorkOrderEditViewModel vm)
-
             {
-
-                // 1. 유효성 검사 실행
-
+                // 1. 유효성 검사 (ViewModel의 검문소를 통과해야 합니다)
                 if (vm.Validate())
-
                 {
-
-                    // 2. 검사 통과: IsSaved를 true로 설정하고 창을 닫습니다.
-
+                    // [통과 시 로직]
+                    // ViewModel에 "이 데이터는 진짜로 저장할 거예요!"라고 신호를 줍니다.
                     vm.IsSaved = true;
 
-                    this.DialogResult = true;// WorkOrderListViewModel로 성공을 알림
+                    // 이 창을 연 메인 화면(부모)에게 "작업 성공!"(true)이라고 알려줍니다.
+                    this.DialogResult = true;
 
+                    // 팝업창을 닫습니다.
                     this.Close();
-
                 }
-
                 else
-
                 {
-
-                    // 3. 검사 실패: 경고 메시지 박스를 띄웁니다.
-
-                    MessageBox.Show(vm.ValidationMessage, "입력오류", MessageBoxButton.OK, MessageBoxImage.Warning);
-
+                    // [탈락 시 로직]
+                    // ViewModel이 정리해둔 "뭐가 잘못됐는지" 메시지를 팝업으로 띄워줍니다.
+                    MessageBox.Show(vm.ValidationMessage, "입력 오류", MessageBoxButton.OK, MessageBoxImage.Warning);
                 }
-
-                //vm.IsSaved = true; // 저장 성공 플래그 설정
-
             }
-
-            //this.Close(); // 팝업 창 닫기
-
         }
 
-
-
+        // ---------------------------------------------------------------------
+        // [취소 버튼] 클릭 시 실행
+        // ---------------------------------------------------------------------
         private void CancelButton_Click(object sender, RoutedEventArgs e)
-
         {
-
-            // 취소 버튼: IsSaved=false (기본값)로 두고 창을 닫습니다.
-
-            // DataContext가 ViewModel일 때만 처리
-
+            // 두뇌(ViewModel)를 가져옵니다.
             if (DataContext is WorkOrderEditViewModel viewModel)
-
             {
-
+                // 저장하지 않을 것이므로 IsSaved를 false로 확실히 해둡니다.
                 viewModel.IsSaved = false;
-
             }
 
-            this.DialogResult = false; // WorkOrderListViewModel로 실패/취소를 알림
+            // 이 창을 연 메인 화면(부모)에게 "그냥 취소했어요"(false)라고 알려줍니다.
+            this.DialogResult = false;
 
+            // 아무 작업 없이 팝업창을 닫습니다.
             this.Close();
-
         }
-
     }
-
 }
